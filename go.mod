7a5797e9c7b794bb80c68ce8e7e3c98df36fe34a
abaquie <earl.john.abaquita@autodesk.com>
2025-01-23 10:57:17 +0800

    training library creation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/earljohn004/stringutils
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package stringutils
+
+import "strings"
+
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func ToLower(s string) string {
+	return strings.ToLower(s)
+}
